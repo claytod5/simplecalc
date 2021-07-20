ec4d561146fabe547e3b68d80c33b484554a2a28
claytod <dr_falken1983@protonmail.com>
2021-07-20 01:28:14 -0400

    Fixed build issue

--- simplecalc/cli.py
+++ simplecalc/cli.py
@@ -2,7 +2,7 @@
 import argparse
 import sys
 
-import simplecalc
+from simplecalc import operators
 
 
 def main():
@@ -19,37 +19,37 @@ def main():
     # Fibonacci sub-command
     fibonacci = subparsers.add_parser("fibonacci", help="Find fibonacci of n")
     fibonacci.add_argument("n", type=int, help="An integer")
-    fibonacci.set_defaults(func=simplecalc.fibonacci)
+    fibonacci.set_defaults(func=operators.fibonacci)
 
     # Factorial sub-command
     factorial = subparsers.add_parser("factorial", help="Find factorial of n")
     factorial.add_argument("n", type=int, help="An integer")
-    factorial.set_defaults(func=simplecalc.factorial)
+    factorial.set_defaults(func=operators.factorial)
 
     # Prime Check sub-command
     prime = subparsers.add_parser("prime", help="Determine if this is a prime")
     prime.add_argument("n", type=int, help="An integer")
-    prime.set_defaults(func=simplecalc.is_prime)
+    prime.set_defaults(func=operators.is_prime)
 
     # Add sub-command
     add = subparsers.add_parser("add", help="Add the integer(s)")
     add.add_argument("n", nargs="+", type=int, help="An integer or integers")
-    add.set_defaults(func=simplecalc.add)
+    add.set_defaults(func=operators.add)
 
     # Subtract sub-command
     subtract = subparsers.add_parser("subtract", help="Subtract the integer(s)")
     subtract.add_argument("n", nargs="+", type=int, help="An integer or integers")
-    subtract.set_defaults(func=simplecalc.subtract)
+    subtract.set_defaults(func=operators.subtract)
 
     # Multiply sub-command
     multiply = subparsers.add_parser("multiply", help="Multiply the integers")
     multiply.add_argument("n", nargs="+", type=int, help="An integer or integers")
-    multiply.set_defaults(func=simplecalc.multiply)
+    multiply.set_defaults(func=operators.multiply)
 
     # Divide sub-command
     divide = subparsers.add_parser("divide", help="Divide the integers")
     divide.add_argument("n", nargs="+", type=int, help="An integer or integers")
-    divide.set_defaults(func=simplecalc.divide)
+    divide.set_defaults(func=operators.divide)
 
     # Power sub-command
     power = subparsers.add_parser(
@@ -58,7 +58,7 @@ def main():
     )
     power.add_argument("x", type=int, help="Base integer")
     power.add_argument("y", type=int, help="Exponent integer")
-    power.set_defaults(func=simplecalc.power)
+    power.set_defaults(func=operators.power)
 
     args = parser.parse_args()
 
